17f9a735c36d295cb20acbcc73f4841dbf6732e6
SUMANTH1010 <ysvsumanth1010@gmail.com>
2024-07-20 11:21:07 +0530

    Style the Avtive Link in the NavBar

--- issue-tracker/app/NavBar.tsx
+++ issue-tracker/app/NavBar.tsx
@@ -1,7 +1,12 @@
+'use client';
 import Link from 'next/link'
+import { usePathname } from 'next/navigation';
 import React from 'react'
 import {AiFillBug} from 'react-icons/ai';
+import classnames from 'classnames';
+
 const navbar = () => {
+    const currentPath = usePathname();
 
     const links = [
         {label : 'Dashboard', href: '/'}, 
@@ -12,7 +17,11 @@ const navbar = () => {
     <Link href="/"><AiFillBug/></Link>
     <ul className='flex space-x-6'>
         {links.map(link => <Link key={link.href} 
-        className='text-zinc-500 hover:text-zinc-800' 
+        className={ classnames({
+            'text-zinc-900' : link.href === currentPath,
+            'text-zinc-500' : link.href !== currentPath,
+            'hover:text-zinc-800 transition-colors' : true
+        })}
         href={link.href}>{link.label}</Link>)}
     </ul>
 </nav>
